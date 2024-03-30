05c95dc168925cb5b00e9fc6d686b97dc83dc0db
Luis <luismineo@edu.unifil.br>
2024-03-30 00:57:31 -0300

    Polish

--- src/main/java/com/invman/inventory/repository/orders/SaleOrderRepository.java
+++ src/main/java/com/invman/inventory/repository/orders/SaleOrderRepository.java
@@ -2,6 +2,8 @@ package com.invman.inventory.repository.orders;
 
 import com.invman.inventory.model.orders.SaleOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SaleOrderRepository extends JpaRepository<SaleOrder, Long> {
 }
--- src/main/java/com/invman/inventory/repository/orders/WorkOrderRepository.java
+++ src/main/java/com/invman/inventory/repository/orders/WorkOrderRepository.java
@@ -2,6 +2,8 @@ package com.invman.inventory.repository.orders;
 
 import com.invman.inventory.model.orders.WorkOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WorkOrderRepository extends JpaRepository<WorkOrder, Long> {
 }
